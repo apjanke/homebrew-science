409c71154b9cda994c494d85275887d4b634f866
Andrew Janke <andrew@apjanke.net>
2016-10-03 12:41:53 -0400

    sumo: revision bump for proj

--- sumo.rb
+++ sumo.rb
@@ -3,6 +3,7 @@ class Sumo < Formula
   homepage "https://sourceforge.net/projects/sumo/"
   url "https://downloads.sourceforge.net/project/sumo/sumo/version%200.25.0/sumo-all-0.25.0.tar.gz"
   sha256 "e56552e4cd997ccab59b5c6828ca1e044e71e3ffe8c780831bf5aa18c5fdd18a"
+  revision 1
 
   bottle do
     cellar :any
